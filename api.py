ad22f89169da7588c0ed599e82e5577d425ed491
Abhishek Singh <abhishek.nitdgp.98@gmail.com>
2018-03-25 16:45:55 +0530

    Add api.py

---
+++ api.py
@@ -0,0 +1,52 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# Create some test data
+atten = [
+    {'subid': 0,
+     'subject': 'Computer org and Architecture',
+     'attendance': 83},
+    {'subid': 1,
+     'subject': 'OOPS',
+     'attendance': 89},
+    {'subid': 2,
+     'subject': 'Discrete Structures',
+     'attendance': 79}
+]
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>My Attendance</h1>
+<p>This is lower than you expect. :P </p>'''
+
+
+@app.route('/api/nit/atten/all', methods=['GET'])
+def api_all():
+    return jsonify(atten)
+
+
+@app.route('/api/nit/atten', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if 'subid' in request.args:
+        id = int(request.args['subid'])
+    else:
+        return "Error: No subject id field provided. Please specify one"
+
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested subjectID.
+    for att in atten:
+        if att['subid'] == id:
+            results.append(att)
+
+    return jsonify(results)
+
+app.run()
